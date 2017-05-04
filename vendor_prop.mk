becaf1086d4baf177ac3261c176029deb8e6885d
YumeMichi <do4suki@gmail.com>
2017-05-04 11:26:44 +0530

    onc: Switch to QTI bluetooth library
    Change-Id: Ic8907cf7f012dcd3ada5715c087b6d591d8aecfe
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -48,6 +48,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.bt.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    ro.bluetooth.a2dp_offload.supported=true \
+    ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.qcom.bluetooth.soc=pronto \
     vendor.bluetooth.soc=pronto
 
