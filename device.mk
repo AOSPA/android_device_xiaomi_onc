89ff22a210e7610d2bc2e460eb69d0337d90ac04
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-09 13:16:27 -0700

    onc: Build vendor version of libstdc++
    Change-Id: Ie43c0a6b594ecfcff393efb8a3937f04af0ee0a4

--- device.mk
+++ device.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
+    libstdc++.vendor \
     Snap
 
 # Configstore
