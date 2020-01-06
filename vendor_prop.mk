e3c2cf0b895c307974a581edc52b8ba650c18389
Saurabh Charde <saurabhchardereal@gmail.com>
2020-01-06 22:38:48 +0530

    onc: init: Set dalvik-heap configurations based on RAM variant
    * Since we have 3 models with different amount of RAM
    
    * Values taken from frameworks/native
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -30,12 +30,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=192m \
-    dalvik.vm.heapmaxfree=8m \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heaptargetutilization=0.75 \
     ro.dalvik.vm.native.bridge=0
 
 # Display
