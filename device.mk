37582b198203329fd9e33fcc4fa57f7745ce0123
Jyotiraditya <jyotiraditya@aospa.co>
2020-01-17 18:29:17 +0530

    onc: Switch to File Based Encryption
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>

--- device.mk
+++ device.mk
@@ -179,10 +179,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service \
     android.hardware.health@2.0-impl
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
