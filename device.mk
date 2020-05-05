21ef1f9b944487bdd195297aeef3cb6676d97933
Jyotiraditya <jyotiraditya@aospa.co>
2020-05-05 19:11:22 +0530

    onc: Move to vendor/pa
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>

--- device.mk
+++ device.mk
@@ -119,7 +119,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     com.qualcomm.qti.bluetooth_audio@1.0 \
     libbluetooth_qti \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
